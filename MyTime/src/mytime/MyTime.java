2b8085b4507db8a629befdd3d594a3a1393792e5
Edgar Ontiveros <eontiveros1@saddleback.edu>
2016-09-01 12:58:55 -0700

    Started using Calendar

--- MyTime/src/mytime/MyTime.java
+++ MyTime/src/mytime/MyTime.java
@@ -6,13 +6,9 @@ public class MyTime {
 
     
     public static void main(String[] args) {
-        //long time = System.currentTimeMillis();
-        // System.out.printf("The current time is %d\n", time);
         Scanner read = new Scanner(System.in);
         long time = read.nextLong();
-        Calendar date = Calendar.getInstance();
-        int dow = date.get(Calendar.DAY_OF_WEEK);
-        System.out.printf("Day of the week%i",dow);
+        System.out.printf("%d\n", dateObject);
     }
     
 }
